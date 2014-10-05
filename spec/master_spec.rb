1d6b0f897c0602c8e9e96157d445d90501359215
brandoshmando <brancraft@gmail.com>
2014-10-05 12:15:38 -0500

    First test for instantiation of master object

--- spec/master_spec.rb
+++ spec/master_spec.rb
@@ -1,5 +1,12 @@
-require_relative '../master_spec.rb'
+require_relative '../master.rb'
 
-describe Mastermind do
+describe Master do
+  let(:master) { Master.new }
 
+
+  describe "#new" do
+    it "creates a new instance of master" do
+      expect(master).to be_instance_of(Master)
+    end
+  end
 end
