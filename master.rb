defed546bfa9e34d319291612c13041d40e5b220
brandoshmando <brancraft@gmail.com>
2014-10-05 14:28:17 -0500

    Test for out put. Assigning a long string to the menu var is only temporary...

--- master.rb
+++ master.rb
@@ -1,2 +1,6 @@
 class Master
+
+  def main_menu
+    puts "Welcome to Mastermind!"
+  end
 end
--- spec/master_spec.rb
+++ spec/master_spec.rb
@@ -4,9 +4,21 @@ describe Master do
   let(:master) { Master.new }
 
 
-  describe "#new" do
+  describe ".new" do
     it "creates a new instance of master" do
       expect(master).to be_instance_of(Master)
+    end
+
+    it "should have method main_menu" do
+      expect(master).to respond_to :main_menu
+    end
+  end
+
+  describe "#main_menu" do
+
+    menu = "Welcome to Mastermind!\n"
+    it "outputs list of options for user" do
+      expect { master.main_menu }.to output(menu).to_stdout
     end
   end
 end
