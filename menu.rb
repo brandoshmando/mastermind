773f4646fb5349b3834ef64815706c670566ce71
brandoshmando <brancraft@gmail.com>
2014-10-06 17:16:11 -0500

    Did not add menu.rb to staged files after it was created

---
+++ menu.rb
@@ -0,0 +1,16 @@
+module Menu
+
+  START = "
+        Welcome to Mastermind
+    ------------------------------
+               Main Menu
+    ------------------------------
+    Select from the following:
+
+    [1] 1 player
+    [2] 2 player
+    [3] vs. Computer
+    [4] Exit
+    ------------------------------
+    "
+end
