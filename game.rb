b9260a5776b5c0c7c7732e4517d66c1a19549368
brandoshmando <brancraft@gmail.com>
2014-10-08 14:17:26 -0500

    Game class initializer complete-ish

--- game.rb
+++ game.rb
@@ -1,9 +1,11 @@
 class Game
-  def initialize
-    @player_one
-    @player_two
-    @total_points
-    @rounds
-    @num_rounds
+  attr_reader :player_one, :player_two
+
+  def initialize(player_one, player_two , num_rounds)
+    @player_one = player_one
+    @player_two = player_one
+    @rounds = []
+    @round_counter = num_rounds
+    # @possible_points = something
   end
 end
